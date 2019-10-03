b4e5122827143e9d1226c7f8239f694924d6844e
Top-Weerapat <weerpat@central.tech>
2019-10-03 13:42:18 +0700

    FIx

--- src/features/posts/hooks/reducer.js
+++ src/features/posts/hooks/reducer.js
@@ -6,7 +6,7 @@ export const initialState = {
 }
 
 
-export const postReducer = (state = initialState, action) => {
+export const postReducer = (state, action) => {
   const { type } = action
   const { setStateWithKeyRequest, setStateWithKeySuccess, setStateWithKeyFailure } = reducerCreator(state, action)
   switch (type) {
